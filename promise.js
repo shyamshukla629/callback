cfa2938d886fc7770bbca8e7a7c2786f5692a713
Aditya Shukla <shyamshukla9578@gmail.com>
2026-06-29 16:52:32 +0530

    promise is resolved

---
+++ promise.js
@@ -0,0 +1,21 @@
+// const cart=["shoes","pants","kurta"];
+// createOrder(cart,function(orderId){
+//     proceedToPayment(orderId);
+// });
+// const promise=createOrder(cart);
+// promise.then(function(orderId){
+//     proceedToPayment(orderId);
+// });
+
+// promise is resolved 
+
+let promise = new Promise((resolve, reject) => {
+    console.log("this is promise");
+    setTimeout(() => {
+        resolve("promise is resolved");
+        console.log("promise is solbed");
+        
+    }, 4000);
+
+    // resolve("promise is resolverd");
+}); 
