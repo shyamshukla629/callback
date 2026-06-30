a242acb0f2abd664f7140201cbbe8423c53646e7
Aditya Shukla <shyamshukla9578@gmail.com>
2026-06-30 15:07:09 +0530

    we have made higher order function using map , filter , and reduce function

---
+++ higherorder.js
@@ -0,0 +1,24 @@
+// map function 
+
+const arr=[52,5,4,8,9,7,];
+// const output=arr.map(function binary(x){
+//     return x.toString(2);
+// });
+// console.log(output);
+ 
+// using arrow function
+// const output=arr.map((x)=>x.toString(2));
+// console.log(output);
+
+// reduce function using arrow method 
+// const output=arr.reduce((prev,curr)=>{
+//    return prev+curr;
+// });
+// console.log(output);
+
+// // filter function using arrow method 
+// const output=arr.filter((x)=>x%2==0);
+// console.log(output);
+
+// const output1=arr.filter((x)=>x>5);
+// console.log(output1);
